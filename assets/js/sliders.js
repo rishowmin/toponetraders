224a04e5b33b9866d670662e72a28bc347f3d480
Muhammad Raisul Islam <raisul.syp@gmail.com>
2023-02-16 17:31:41 +0600

    client section

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -20,12 +20,39 @@ $(document).ready(function () {
     $('.testimonials_slider').owlCarousel({
         loop: true,
         margin: 0,
-        nav: false,
+        nav: true,
         dots: false,
         items: 1,
         mouseDrag: true,
-        autoplay: false,
+        navText: ["<span aria-label='Previous'><i class='bx bx-left-arrow-alt'></i></span>", "<span aria-label='Next'><i class='bx bx-right-arrow-alt'></i></span>"],
+        autoplay: true,
         autoplayTimeout: 5000,
     })
     // Testimonials end
+
+    
+    // Clients start
+    $('.clients_slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        mouseDrag: true,
+        navText: ["<span aria-label='Previous'><i class='bx bx-chevron-left'></i></span>", "<span aria-label='Next'><i class='bx bx-chevron-right'></i></span>"],
+        autoplay: false,
+        autoplayTimeout: 5000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    
+    })
+    // Clients end
 });
